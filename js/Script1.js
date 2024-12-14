fe8b13ec26c11c8c61090fa291524b4cf34ddb0c
minhtriet13042008 <miktrkkj1304@gmail.com>
2024-12-14 16:02:47 +0700

    Add files via upload

---
+++ js/Script1.js
@@ -0,0 +1,22 @@
+﻿function Kiemtra(ten , mail){
+	var ten = document.getElementById("ten").value;
+	var email = document.getElementById("mail").value;
+	var tel = document.getElementById("tel").value;
+	if(ten == "")
+		{
+		alert("Chua nhap ten");
+		return false;
+		}
+	if(email == "")
+		{
+		alert("Chua nhap email");
+		return false;
+		}
+	if(tel == "")
+		{
+		alert("Chua nhap tel");
+		return false;
+		}
+	alert("Form duoc gui thanh cong.");
+	event.preventDefault();
+	return true;}
